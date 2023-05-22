272aa5a266969cb808039b3753bdc56803427f1a
amirhossein allami <captain2480722023@gmail.com>
2023-05-22 19:05:04 +0330

    add File

---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿let $ = document
+let contextMenu = $.getElementById('contextMenu')
+let body = $.body
+
+body.addEventListener('contextmenu', function (event) {
+
+    event.preventDefault()
+
+    contextMenu.style.display = 'block'
+    contextMenu.style.left = event.pageX
+    contextMenu.style.top = event.pageY
+})
+body.addEventListener('click', function () {
+    contextMenu.style.display = 'none'
+})
+body.addEventListener('keypress', function (event) {
+    if (event.code === "Escape") {
+        contextMenu.style.display = 'none'
+    }
+})
